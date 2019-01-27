09b45e40de03cf7fedcdd86e0e2e17f0982e0a38
farofeiro231 <victoremanoel23@gmail.com>
2019-01-27 20:47:43 +0100

    ...

---
+++ Learning_kivy/hello_world.py
@@ -0,0 +1,14 @@
+import kivy
+#kivy.require('1.7.0')
+
+from kivy.app import App
+from kivy.uix.button import Label
+
+
+class HelloApp(App):
+    def build(self):
+        return Label(text='Hello World! Foda-se!')
+
+
+if __name__=="__main__":
+    HelloApp().run()
